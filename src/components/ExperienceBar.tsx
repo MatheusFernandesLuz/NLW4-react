e9c403638e601bba4895aa8a00aa146bf78a8eb6
Matheus Luz <matheusfernandesluz@gmail.com>
2021-02-23 15:39:44 -0400

    First commit - aula 02

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,18 @@
+import styles from "../styles/components/ExperienceBar.module.css";
+
+const ExperienceBar = () => {
+  return (
+    <header className={styles.experienceBar}>
+      <span>0xp </span>
+      <div>
+        <div style={{ width: "60%" }} />
+        <span className={styles.currentExperience} style={{ left: "60%" }}>
+          300xp
+        </span>
+      </div>
+      <span>600xp</span>
+    </header>
+  );
+};
+
+export default ExperienceBar;
